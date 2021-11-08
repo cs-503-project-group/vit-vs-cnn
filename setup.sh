f0deb489969a014f0995cb385da4951453ab79a3
PawelMlyniec <01121788@pw.edu.pl>
2021-11-08 11:18:09 +0100

    resolved access issues

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 
 python generate_synset_ids.py $1 $2
 git clone  https://github.com/mf1024/ImageNet-datasets-downloader
-cd ImageNet-Datasets-Downloader
+cd ImageNet-datasets-downloader
 mkdir data
 mkdir data/ID
 mkdir data/OOD
 cd ..
-mv ood_download.sh ImageNet-Datasets-Downloader/
-mv id_download.sh ImageNet-Datasets-Downloader/
+mv ood_download.sh ImageNet-datasets-downloader/
+mv id_download.sh ImageNet-datasets-downloader/
 pip install -r requirements.txt 
-cd ImageNet-Datasets-Downloader
+cd ImageNet-datasets-downloader
 pip install -r requirements.txt 
+chmod 777 ood_download.sh
+chmod 777 id_download.sh
 ./ood_download.sh
 ./id_download.sh
