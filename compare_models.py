46ca8745d71d3b2148a8ca12a6cfd5c5093a9c80
matheus-bernat <matheus.bernat@gmail.com>
2021-11-26 11:09:20 +0100

    ID comparison

--- compare_models.py
+++ compare_models.py
@@ -8,11 +8,11 @@ from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 import os
 from eval_utils import evaluate_OOD_detection
+import json
+
 
 from torch import cuda
 device = 'cuda' if cuda.is_available() else 'cpu'
-
-
 
 
 class Image_Dataset(Dataset):
@@ -27,19 +27,37 @@ class Image_Dataset(Dataset):
         for folder in os.listdir(self.ood_data_dir)[:1]:
             for file in os.listdir(self.ood_data_dir+folder)[:10]:
                 self.data.append((self.ood_data_dir+f"{folder}/"+file, 1))
-        
-        
 
     def __getitem__(self, idx):
-        
         img_path, target = self.data[idx]
         img = ori_preprocess(Image.open(img_path))
-
-
         return img, target
 
     def __len__(self):
         return len(self.data)
+
+
+class Image_Dataset_ID(Dataset):
+    def __init__(self, id_data_dir):
+        self.id_data_dir = id_data_dir
+        self.data = []
+        with open('classes_imagenet/ground_truth_labels_validation_1k.json') as f_in:
+            self.gt_labels = json.load(f_in)
+        
+        for file in os.listdir(self.id_data_dir):
+            gt_label = self.gt_labels[file]
+            self.data.append((self.id_data_dir+file, gt_label))
+
+    def __get__item__(self, idx):
+        img_path, target = self.data[idx]
+        img = ori_preprocess(Image.open(img_path))
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+
+
 
 
 ood_data_dir = "/home/SHARED_FOLDER/data/OOD_data/"
@@ -50,12 +68,17 @@ ori_preprocess = Compose([
         CenterCrop(size=(224, 224)),
         ToTensor()])
 
+# OOD evaluation
 dataset = Image_Dataset(id_data_dir, ood_data_dir)
-data_loader = DataLoader(dataset, batch_size = 1, shuffle= True)
+data_loader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+# ID evaluation
+dataset_id = Image_Dataset_ID(id_data_dir, ood_data_dir)
+data_loader_ID = DataLoader(dataset, batch_size=1, shuffle=True)
 
 thresholds = torch.range(0.5, 0.9, step=0.1)
 
-# ResNet
+# --------------------------------------- ResNet ---------------------------------------
 ood_threshold = 0.5
 resnet_model = resnet.ResNet().to(device)
 # the goal is to identify OOD samples
@@ -65,27 +88,27 @@ print(resnet_prc, resnet_rec, resnet_f1)
 
 
 
-# DeiT
+# --------------------------------------- DeiT ---------------------------------------
 deit_model = deit.DeiT().to(device)
 
 deit_prc, deit_rec, deit_f1 = evaluate_OOD_detection(deit_model, data_loader, thresholds, device)
 print(deit_prc, deit_rec, deit_f1)
 
 
-# ConvMixer
+# --------------------------------------- ConvMixer ---------------------------------------
 convmixer_model = convmixer.ConvMixer().to(device)
 
 convmixer_prc, convmixer_rec, convmixer_f1 = evaluate_OOD_detection(convmixer_model, data_loader, thresholds, device)
 print(convmixer_prc, convmixer_rec, convmixer_f1)
 
 
-# MLPMixer
+# --------------------------------------- MLPMixer ---------------------------------------
 mlpmixer_model = mlpmixer.MLPMixer().to(device)
 
 mlpmixer_prc, mlpmixer_rec, mlpmixer_f1 = evaluate_OOD_detection(mlpmixer_model, data_loader, thresholds, device)
 print(mlpmixer_prc, mlpmixer_rec, mlpmixer_f1)
 
-# EcaResNet
+# --------------------------------------- EcaResNet ---------------------------------------
 ecaresnet_model = ecaresnet.ECAResNet().to(device)
 ecaresnet_prc, ecaresnet_rec, ecaresnet_f1 = evaluate_OOD_detection(ecaresnet_model, data_loader, thresholds, device)
 print(ecaresnet_prc, ecaresnet_rec, ecaresnet_f1)
--- eval_utils.py
+++ eval_utils.py
@@ -1,6 +1,7 @@
 import torch
 from torchmetrics import Precision, Recall, F1
-
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
 
 
 def evaluate_OOD_detection(model, dataloader, thresholds, device, num_classes=1000, batch_size=1):
@@ -24,7 +25,6 @@ def evaluate_OOD_detection(model, dataloader, thresholds, device, num_classes=10
                 probs = batch_probs
                 targets = batch_target
             else:
-                
                 probs = torch.cat((probs, batch_probs), dim=0)
                 targets = torch.cat((targets, batch_target), dim=0)
         
@@ -40,8 +40,49 @@ def evaluate_OOD_detection(model, dataloader, thresholds, device, num_classes=10
         precisions[ind] = prc(preds, targets)
         recalls[ind] = rec(preds, targets)
         f1_scores[ind] = f1(preds, targets)
-       
 
     return precisions, recalls, f1_scores
 
 
+def evaluate_ID_detection(model, dataloader, thresholds, device, num_classes=1000, batch_size=1):
+
+    probs = torch.zeros((batch_size, num_classes))
+    targets = torch.zeros((batch_size, 1))
+    model.to(device)
+    model.eval()
+   
+    # check value for threshold
+    # prc = Precision(num_classes=num_classes, threshold=1)
+    # rec = Recall(num_classes=num_classes, threshold=1)
+    # f1  = F1(num_classes=num_classes, threshold=1)
+
+    with torch.no_grad():
+        for ind, batch in enumerate(dataloader):
+            inputs, batch_target = batch[0], batch[1]
+            inputs.to(device)
+            
+            batch_probs = model(inputs)
+            if ind == 0:
+                probs = batch_probs
+                targets = batch_target
+            else:
+                probs = torch.cat((probs, batch_probs), dim=0)
+                targets = torch.cat((targets, batch_target), dim=0)
+        
+    precisions = torch.zeros((len(thresholds), 1))
+    recalls    = torch.zeros((len(thresholds), 1))
+    f1_scores  = torch.zeros((len(thresholds), 1))
+
+    for ind, t in enumerate(thresholds):
+        preds = torch.max(probs, dim=1).index + 1 # the number (index + 1) is the number of the class with maximum probability, which goes from 1 to 1_000
+        preds = preds.to(torch.int8)
+        
+        np_preds = preds.cpu().detach().numpy()
+        np_targets = targets.cpu().detach().numpy()
+
+        p, r, f = precision_recall_fscore_support(targets, preds, average='macro')
+        precisions[ind] = p
+        recalls[ind]    = r
+        f1_scores[ind]  = f
+
+    return precisions, recalls, f1_scores
