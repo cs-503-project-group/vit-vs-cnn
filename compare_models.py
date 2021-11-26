e1a098604e13cd3a5f161df97cc652bb9c08e5a4
Sepideh Mamooler <mamooler@izar.ib0.cm.izar.local>
2021-11-26 16:53:37 +0100

    working version \o/

--- compare_models.py
+++ compare_models.py
@@ -7,9 +7,9 @@ from torchmetrics import Precision
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 import os
-from eval_utils import evaluate_OOD_detection
+from eval_utils import evaluate_OOD_detection, evaluate_ID_detection
 import json
-
+import pickle
 
 from torch import cuda
 device = 'cuda' if cuda.is_available() else 'cpu'
@@ -34,24 +34,37 @@ class Image_Dataset(Dataset):
     def __getitem__(self, idx):
         img_path, target = self.data[idx]
         img = None
-        with Image.open(img_path) as im:
+        with Image.open(img_path).convert('RGB') as im:
             img = ori_preprocess(im)
+
+        return img, target
+
+
+    def __len__(self):
+        return len(self.data)
 
 
 class Image_Dataset_ID(Dataset):
     def __init__(self, id_data_dir):
         self.id_data_dir = id_data_dir
         self.data = []
-        with open('classes_imagenet/ground_truth_labels_validation_1k.json') as f_in:
+        self.classes = []
+        with open('vit-vs-cnn/classes_imagenet/ground_truth_labels_validation_1k.json') as f_in:
             self.gt_labels = json.load(f_in)
         
-        for file in os.listdir(self.id_data_dir):
-            gt_label = self.gt_labels[file]
-            self.data.append((self.id_data_dir+file, gt_label))
+        for folder in os.listdir(self.id_data_dir):
+            for file in os.listdir(self.id_data_dir+folder)[:10]:
+                gt_label = self.gt_labels[file[:-5]]
+                if gt_label not in self.classes:
+                    self.classes.append(gt_label)
 
-    def __get__item__(self, idx):
+                self.data.append((self.id_data_dir+folder+"/"+file, gt_label))
+
+    def __getitem__(self, idx):
         img_path, target = self.data[idx]
-        img = ori_preprocess(Image.open(img_path))
+        with Image.open(img_path).convert('RGB') as im:
+            img = ori_preprocess(im)
+        
         return img, target
 
     def __len__(self):
@@ -71,26 +84,58 @@ dataset = Image_Dataset(id_data_dir, ood_data_dir)
 data_loader = DataLoader(dataset, batch_size=1, shuffle=True)
 
 # ID evaluation
-dataset_id = Image_Dataset_ID(id_data_dir, ood_data_dir)
-data_loader_ID = DataLoader(dataset, batch_size=1, shuffle=True)
+dataset_id = Image_Dataset_ID(id_data_dir)
+data_loader_ID = DataLoader(dataset_id, batch_size=1, shuffle=True)
 
 thresholds = np.arange(0.5, 0.9, step=0.1)
 
 # --------------------------------------- ResNet ---------------------------------------
-ood_threshold = 0.5
 resnet_model = resnet.ResNet().to(device)
 # the goal is to identify OOD samples
+ood_resnet_prc, ood_resnet_rec, ood_resnet_f1 = evaluate_OOD_detection(resnet_model, data_loader, thresholds, device)
+id_resnet_prc, id_resnet_rec, id_resnet_f1 = evaluate_ID_detection(resnet_model, data_loader_ID, dataset_id.classes, device)
 
-resnet_prc, resnet_rec, resnet_f1 = evaluate_OOD_detection(resnet_model, data_loader, thresholds, device)
-print(resnet_prc, resnet_rec, resnet_f1)
+with open('ood_resnet_prc.pickle', 'wb') as f:
+    pickle.dump(ood_resnet_prc, f)
+with open('ood_resnet_rec.pickle', 'wb') as f:
+    pickle.dump(ood_resnet_rec, f)
+with open('ood_resnet_f1.pickle', 'wb') as f:
+    pickle.dump(ood_resnet_f1, f)
+
+with open('id_resnet_prc.pickle', 'wb') as f:
+    pickle.dump(id_resnet_prc, f)
+with open('id_resnet_rec.pickle', 'wb') as f:
+    pickle.dump(id_resnet_rec, f)
+with open('id_resnet_f1.pickle', 'wb') as f:
+    pickle.dump(id_resnet_f1, f)
+
+print(id_resnet_prc, id_resnet_rec, id_resnet_f1)
+print(ood_resnet_prc, ood_resnet_rec, ood_resnet_f1)
 
 
 
 # --------------------------------------- DeiT ---------------------------------------
 deit_model = deit.DeiT().to(device)
 
-deit_prc, deit_rec, deit_f1 = evaluate_OOD_detection(deit_model, data_loader, thresholds, device)
-print(deit_prc, deit_rec, deit_f1)
+ood_deit_prc, ood_deit_rec, ood_deit_f1 = evaluate_OOD_detection(deit_model, data_loader, thresholds, device)
+id_deit_prc, id_deit_rec, id_deit_f1 = evaluate_ID_detection(deit_model, data_loader_ID, dataset_id.classes, device)
+
+with open('ood_deit_prc.pickle', 'wb') as f:
+    pickle.dump(ood_deit_prc, f)
+with open('ood_deit_rec.pickle', 'wb') as f:
+    pickle.dump(ood_deit_rec, f)
+with open('ood_deit_f1.pickle', 'wb') as f:
+    pickle.dump(ood_deit_f1, f)
+
+with open('id_deit_prc.pickle', 'wb') as f:
+    pickle.dump(id_deit_prc, f)
+with open('id_deit_rec.pickle', 'wb') as f:
+    pickle.dump(id_deit_rec, f)
+with open('id_deit_f1.pickle', 'wb') as f:
+    pickle.dump(id_deit_f1, f)
+
+print(id_deit_prc, id_deit_rec, id_deit_f1)
+print(ood_deit_prc, ood_deit_rec, ood_deit_f1)
 
 
 # ConvMixer
@@ -102,10 +147,47 @@ print(deit_prc, deit_rec, deit_f1)
 
 # --------------------------------------- MLPMixer ---------------------------------------
 mlpmixer_model = mlpmixer.MLPMixer().to(device)
-mlpmixer_prc, mlpmixer_rec, mlpmixer_f1 = evaluate_OOD_detection(mlpmixer_model, data_loader, thresholds, device)
-print(mlpmixer_prc, mlpmixer_rec, mlpmixer_f1)
+
+ood_mlpmixer_prc, ood_mlpmixer_rec, ood_mlpmixer_f1 = evaluate_OOD_detection(mlpmixer_model, data_loader, thresholds, device)
+id_mlpmixer_prc, id_mlpmixer_rec, id_mlpmixer_f1 = evaluate_ID_detection(mlpmixer_model, data_loader_ID, dataset_id.classes, device)
+
+with open('ood_mlpmixer_prc.pickle', 'wb') as f:
+    pickle.dump(ood_mlpmixer_prc, f)
+with open('ood_mlpmixer_rec.pickle', 'wb') as f:
+    pickle.dump(ood_mlpmixer_rec, f)
+with open('ood_mlpmixer_f1.pickle', 'wb') as f:
+    pickle.dump(ood_mlpmixer_f1, f)
+
+with open('id_mlpmixer_prc.pickle', 'wb') as f:
+    pickle.dump(id_mlpmixer_prc, f)
+with open('id_mlpmixer_rec.pickle', 'wb') as f:
+    pickle.dump(id_mlpmixer_rec, f)
+with open('id_mlpmixer_f1.pickle', 'wb') as f:
+    pickle.dump(id_mlpmixer_f1, f)
+
+print(id_mlpmixer_prc, id_mlpmixer_rec, id_mlpmixer_f1)
+print(ood_mlpmixer_prc, ood_mlpmixer_rec, ood_mlpmixer_f1)
 
 # --------------------------------------- EcaResNet ---------------------------------------
 ecaresnet_model = ecaresnet.ECAResNet().to(device)
-ecaresnet_prc, ecaresnet_rec, ecaresnet_f1 = evaluate_OOD_detection(ecaresnet_model, data_loader, thresholds, device)
-print(ecaresnet_prc, ecaresnet_rec, ecaresnet_f1)
+
+ood_ecaresnet_prc, ood_ecaresnet_rec, ood_ecaresnet_f1 = evaluate_OOD_detection(ecaresnet_model, data_loader, thresholds, device)
+id_ecaresnet_prc, id_ecaresnet_rec, id_ecaresnet_f1 = evaluate_ID_detection(ecaresnet_model, data_loader_ID, dataset_id.classes, device)
+
+
+with open('ood_ecaresnet_prc.pickle', 'wb') as f:
+    pickle.dump(ood_ecaresnet_prc, f)
+with open('ood_ecaresnet_rec.pickle', 'wb') as f:
+    pickle.dump(ood_ecaresnet_rec, f)
+with open('ood_ecaresnet_f1.pickle', 'wb') as f:
+    pickle.dump(ood_ecaresnet_f1, f)
+
+with open('id_ecaresnet_prc.pickle', 'wb') as f:
+    pickle.dump(id_ecaresnet_prc, f)
+with open('id_ecaresnet_rec.pickle', 'wb') as f:
+    pickle.dump(id_ecaresnet_rec, f)
+with open('id_ecaresnet_f1.pickle', 'wb') as f:
+    pickle.dump(id_ecaresnet_f1, f)
+
+print(id_ecaresnet_prc, id_ecaresnet_rec, id_ecaresnet_f1)
+print(ood_ecaresnet_prc, ood_ecaresnet_rec, ood_ecaresnet_f1)
--- eval_utils.py
+++ eval_utils.py
@@ -14,6 +14,7 @@ def evaluate_OOD_detection(model, dataloader, thresholds, device, num_classes=10
   
     with torch.no_grad():
         for ind, batch in enumerate(dataloader):
+            
             inputs, batch_target = batch[0].cuda(), batch[1]
             inputs.to(device)
             
@@ -46,45 +47,37 @@ def evaluate_OOD_detection(model, dataloader, thresholds, device, num_classes=10
     return precisions, recalls, f1_scores
 
 
-def evaluate_ID_detection(model, dataloader, thresholds, device, num_classes=1000, batch_size=1):
+def evaluate_ID_detection(model, dataloader, classes, device, num_classes=1000, batch_size=1):
 
-    probs = torch.zeros((batch_size, num_classes))
-    targets = torch.zeros((batch_size, 1))
+    probs = np.zeros((batch_size, num_classes))
+    targets = np.zeros((batch_size, 1))
+    
     model.to(device)
     model.eval()
    
-    # check value for threshold
-    # prc = Precision(num_classes=num_classes, threshold=1)
-    # rec = Recall(num_classes=num_classes, threshold=1)
-    # f1  = F1(num_classes=num_classes, threshold=1)
 
     with torch.no_grad():
         for ind, batch in enumerate(dataloader):
-            inputs, batch_target = batch[0], batch[1]
+            inputs, batch_target = batch[0].cuda(), batch[1]
             inputs.to(device)
             
             batch_probs = model(inputs)
             if ind == 0:
-                probs = batch_probs
-                targets = batch_target
+                probs = batch_probs.cpu().numpy()
+                targets = batch_target.cpu().numpy()
             else:
-                probs = torch.cat((probs, batch_probs), dim=0)
-                targets = torch.cat((targets, batch_target), dim=0)
+
+                probs = np.concatenate((probs, batch_probs.cpu().numpy()), axis=0)
+                targets = np.append(targets, batch_target.cpu().numpy())
         
-    precisions = torch.zeros((len(thresholds), 1))
-    recalls    = torch.zeros((len(thresholds), 1))
-    f1_scores  = torch.zeros((len(thresholds), 1))
+    
+    
+    preds = np.argmax(probs, axis=1) + 1 # the number (index + 1) is the number of the class with maximum probability, which goes from 1 to 1_000
+    print(preds)
+    print(targets)
+    
+    prc, rec, f1, _ = precision_recall_fscore_support(targets, preds, labels= classes, average='macro')
+    
+   
 
-    for ind, t in enumerate(thresholds):
-        preds = torch.max(probs, dim=1).index + 1 # the number (index + 1) is the number of the class with maximum probability, which goes from 1 to 1_000
-        preds = preds.to(torch.int8)
-        
-        np_preds = preds.cpu().detach().numpy()
-        np_targets = targets.cpu().detach().numpy()
-
-        p, r, f = precision_recall_fscore_support(targets, preds, average='macro')
-        precisions[ind] = p
-        recalls[ind]    = r
-        f1_scores[ind]  = f
-
-    return precisions, recalls, f1_scores
+    return prc, rec, f1
